02cae81f336c2c1f12d47f281c76c3d71552dbe6
侯少朋 <hsp_ntes@163.com>
2020-03-04 17:42:33 +0800

    test第一次提交

---
+++ Test/src/cn/itheima/ITest.java
@@ -0,0 +1,13 @@
+package cn.itheima;
+
+public interface ITest {
+    int a = 2;
+
+    //final int b;//final 类型的变量需要给出初始值
+
+    abstract void show();
+
+    private void test(){
+
+    };
+}
---
+++ Test/src/cn/itheima/Test.java
@@ -0,0 +1,37 @@
+package cn.itheima;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.*;
+/*public class Test implements Comparable<Integer>{
+    public static void main(String[] args) {
+        *//*同步代码块
+        Object obj=new Object();
+       synchronized (obj){     }*//*
+    }
+    @Override
+    public int compareTo(Integer o) {
+        return 0;
+    }
+}*/
+/*
+public class Test implements Comparator {
+    public static void main(String[] args) {
+        */
+/*同步代码块
+        Object obj=new Object();
+       synchronized (obj){     }*//*
+    }
+    @Override
+    public  int compare(Object o1, Object o2) {
+        return ;
+    }
+}*/
+public class Test {
+    public static void main(String[] args) {
+       /*String s=null;
+       if((s!=null)&&(s.length()>0)){
+           System.out.println("成功打印");*/
+
+    }
+}
